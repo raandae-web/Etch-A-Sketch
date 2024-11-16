758d3791d5706bb64bf3891e2f212188ad6be8e8
raandae-web <chidoziee123@gmail.com>
2024-11-15 20:14:46 -0600

    Functional Etch + Disappearing Etch
    Etch-A-Sketch is now functional, and it also disappears when you click the button and insert a value. I now need to figure out a way to both return the amount of square per side user requested, and have those squares fill the space of the container. rende out.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,46 @@
 
 let boxContainer = document.querySelector(".boxContainer")
 
+let btn = document.querySelector(".btn")
 
-for(let i = 0; i <= 16; i++){
-let boX =  document.createElement("div")
+
+btn.addEventListener("click", ()=>{
+    let userAnswer = prompt("Squares per side?");
+
+    let uASq = userAnswer * userAnswer;
+    if (userAnswer >= 0){
+        while (boxContainer.firstChild) {
+            boxContainer.removeChild(boxContainer.firstChild);
+        }
+        
+        for (let i = 0; i >= uASq; i++){
+            let boX =  document.createElement("div");
+
+            boX.style.width = "50px";
+            boX.style.height = "50px";
+            boX.style.backgroundColor = "black";
+            boX.classList.add("boxes")
+            boxContainer.appendChild(boX)
+
+            boX.addEventListener("mouseover", ()=>{
+                boX.style.backgroundColor = "white";
+            });
+        }
+    }
+
+})
+
+for(let i = 0; i <= 255; i++){
+let boX =  document.createElement("div");
+
 boX.style.width = "50px";
 boX.style.height = "50px";
 boX.style.backgroundColor = "black";
 boX.classList.add("boxes")
 boxContainer.appendChild(boX)
+
+boX.addEventListener("mouseover", ()=>{
+    boX.style.backgroundColor = "white";
+});
 
 }
